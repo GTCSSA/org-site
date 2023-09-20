2ae0bb286c7fc0d43b468631ed30f025bd803b19
cdeng65 <cdeng65@gatech.edu>
2023-09-20 00:38:21 -0400

    remove email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,12 @@ import React from 'react'
 import { FaGithub } from '@react-icons/all-files/fa/FaGithub'
 import { FaInstagram } from '@react-icons/all-files/fa/FaInstagram'
 import { FaFacebook } from '@react-icons/all-files/fa/FaFacebook'
-import { AiFillMail } from '@react-icons/all-files/Ai/AiFillMail'
 import '../styles/footer.css'
 
 function Footer() {
   return (
     <div className='footer'>
     <div className='icons-container'>
-      <a className="icon" href="mailto:gtcssa@gmail.com" target="_blank" rel="noreferrer" aria-label='email'><AiFillMail/></a>
       <a className="icon" href="https://github.com/gtcssa/" target="_blank" rel="noreferrer" aria-label="github"><FaGithub /></a>
       <a className="icon" href="https://www.facebook.com/gtcssa/" target="_blank" rel="noreferrer" aria-label="facebook"><FaFacebook /></a>
       <a className="icon" href="https://www.instagram.com/gtcssa/?hl=en" target="_blank" rel="noreferrer" aria-label="instagram"><FaInstagram /></a>
